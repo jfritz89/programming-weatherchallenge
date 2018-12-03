3b582b1b439d41cf3aca6d271f59aa04132c6425
jfritz89 <45332116+jfritz89@users.noreply.github.com>
2018-12-03 18:47:19 +0100

    Add files via upload
    Aufteilung der Methode in zwei einzelne Methoden

--- src/main/code/challenge/GetDifferenz.java
+++ src/main/code/challenge/GetDifferenz.java
@@ -7,10 +7,62 @@ import java.util.Scanner;
 
 public class GetDifferenz {
 
-	@SuppressWarnings({ "resource", "unused" })
-	public static String getDifferenz(String file, String fname) throws FileNotFoundException {
+	@SuppressWarnings({ "resource" })
+	public static String getDifferenzWeather(String file) throws FileNotFoundException {
 
-		String filename = fname;
+		String result;
+		String result_new = "";
+		int max, min;
+		int differenz = 1000;
+		int differenz_new = 0;
+
+		// Load CSV File
+		String csvFile = "./src/main/resources/de/exxcellent/challenge/" + file;
+
+		// Assign a scanner to the file
+		Scanner scannerLine = new Scanner(new File(csvFile));
+
+		// Ignore header
+		scannerLine.nextLine();
+
+		try {
+
+			// Scan of the rows
+			while (scannerLine.hasNextLine()) {
+
+				// Scan of the columns (first three columns)
+				Scanner scanner = new Scanner(scannerLine.nextLine()).useDelimiter(",");
+
+				result = Integer.toString(scanner.nextInt());
+				max = scanner.nextInt();
+				min = scanner.nextInt();
+
+				// Calculation of the minimum value
+				differenz_new = max - min;
+
+				// Comparison with value from previous row
+				if (differenz_new < differenz) {
+
+					differenz = differenz_new;
+					result_new = result;
+
+				}
+
+			}
+		} catch (InputMismatchException e) {
+
+			System.err.printf("data format ", e);
+			e.getStackTrace();
+
+		}
+
+		return result_new;
+
+	}
+
+	@SuppressWarnings({ "resource" })
+	public static String getDifferenzFootball(String file) throws FileNotFoundException {
+
 		String result;
 		String result_new = "";
 		int max, min;
@@ -34,27 +86,16 @@ public class GetDifferenz {
 				// Scan of the columns (first three columns)
 				Scanner scanner = new Scanner(scannerLine.nextLine()).useDelimiter(",");
 
-				if (fname == "Footballteams") {
+				result = scanner.next();
+				scanner.nextInt();
+				scanner.nextInt();
+				scanner.nextInt();
+				scanner.nextInt();
+				max = scanner.nextInt();
+				min = scanner.nextInt();
 
-					result = scanner.next();
-					scanner.nextInt();
-					scanner.nextInt();
-					scanner.nextInt();
-					scanner.nextInt();
-					max = scanner.nextInt();
-					min = scanner.nextInt();
-
-					// Calculation of the absolute value (min)
-					differenz_new = Math.abs(max - min);
-
-				} else {
-					result = Integer.toString(scanner.nextInt());
-					max = scanner.nextInt();
-					min = scanner.nextInt();
-
-					// Calculation of the minimum value
-					differenz_new = max - min;
-				}
+				// Calculation of the absolute value (min)
+				differenz_new = Math.abs(max - min);
 
 				// Comparison with value from previous row
 				if (differenz_new < differenz) {
@@ -67,7 +108,7 @@ public class GetDifferenz {
 			}
 		} catch (InputMismatchException e) {
 
-			System.err.printf("Dateiformat flasch", e);
+			System.err.printf("Dateiformat falsch", e);
 			e.getStackTrace();
 
 		}
